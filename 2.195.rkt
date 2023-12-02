d72851553fa36f5970a583911b0a5ea6fb7199ab
ihaaae <2497377281@qq.com>
2023-12-02 18:26:26 +0800

    Finish the Exercise with all data definition, purpose statement and test

--- 2.195.rkt
+++ 2.195.rkt
@@ -1,24 +1,34 @@
-;; The first three lines of this file were inserted by DrRacket. They record metadata
-;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-intermediate-reader.ss" "lang")((modname |2.195|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
-
+#lang racket
+(require lang/htdp-intermediate-lambda)
 (require 2htdp/batch-io)
+(require test-engine/racket-tests)
 
 (define LOCATION "words.txt")
 ; On LINUX: /usr/share/dict/words or /var/lib/dict/words
-; On WINDOWS: borrow the word file from your Linux friend
  
 ; A Dictionary is a List-of-strings.
 (define AS-LIST (read-lines LOCATION))
 
+; A Letter is one of the following 1Strings: 
+; – "a"
+; – ... 
+; – "z"
+; or, equivalently, a member? of this list: 
+(define LETTERS
+  (explode "abcdefghijklmnopqrstuvwxyz"))
+
 ;letter dictionary -> number
+;the number of words in _dict_ that start with Letter _l_
 (define (start-with# l dict)
   (cond
     [(empty? dict) 0]
     [else (+
-           (if (string-ci=? (string-ith (first dict) 0) l)
+           (if (string-ci=? (substring (first dict) 0 1) l)
                1
                0)
            (start-with# l (rest dict)))]))
 
-(check-expect (start-with# "e" AS-LIST) 576)
+(define dict (list "apple" "after" "ban"))
+(check-expect (start-with# "a" dict) 2)
+(check-expect (start-with# "c" dict) 0)
+(test)
