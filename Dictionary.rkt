12d2f9b37c331e028b25d664055a3d28b3626370
ihaaae <2497377281@qq.com>
2023-12-03 16:55:43 +0800

    Solution of exercise 197

--- Dictionary.rkt
+++ Dictionary.rkt
@@ -2,6 +2,7 @@
 (require lang/htdp-intermediate-lambda)
 (require 2htdp/batch-io)
 (require test-engine/racket-tests)
+(require racket/trace)
 
 (define LOCATION "words.txt")
 ; On LINUX: /usr/share/dict/words or /var/lib/dict/words
@@ -19,14 +20,16 @@
 
 ;letter dictionary -> number
 ;the number of words in _dict_ that start with Letter _l_
+
 (define (start-with# l dict)
-  (cond
-    [(empty? dict) 0]
-    [else (+
-           (if (string-ci=? (substring (first dict) 0 1) l)
-               1
-               0)
-           (start-with# l (rest dict)))]))
+  (match dict
+    ['() 0]
+    [(cons head tail)
+     (let ([dict-letter (substring head 0 1)]
+           [result (start-with# l tail)])
+       (if (string-ci=? dict-letter l)
+           (+ 1 result)
+           result))]))
 
 (define dict (list "apple" "after" "ban"))
 (check-expect (start-with# "a" dict) 2)
@@ -34,25 +37,57 @@
 
 (struct letter-count (letter count) #:transparent)
 
-
 ;dictionary -> [list-of letter-count]
 ;list of letter-count for count showed in _dict_
 (define (count-by-letter dict)
   (match dict
     ['() '()]
-    [(cons head '()) (cons (letter-count (string-ith head 0) 1) '())]
+    [(cons head '()) (cons (letter-count (substring head 0 1) 1) '())]
     [(cons head tail)
-    (match-let* ([result (count-by-letter tail)]
-                 [dict-letter (string-ith head 0)]
-                 [(cons (letter-count lc-letter lc-count) result-tail) result])
-      (if (string=? dict-letter lc-letter)
-          (cons (letter-count lc-letter (+ lc-count 1))
-                result-tail)
-          (cons (letter-count dict-letter 1)
-                result)))]))
+     (match-let* ([result (count-by-letter tail)]
+                  [dict-letter (substring head 0 1)]
+                  [(cons (letter-count lc-letter lc-count) result-tail) result])
+       (if (string-ci=? dict-letter lc-letter)
+           (cons (letter-count lc-letter (+ lc-count 1))
+                 result-tail)
+           (cons (letter-count dict-letter 1)
+                 result)))]))
 
-(check-expect (count-by-letter '()) '())
-(check-expect (count-by-letter (list "apple")) (list (letter-count "a" 1)))
-(check-expect (count-by-letter dict) (list (letter-count "a" 2) (letter-count "b" 1)))
+;[list-of string] [list-of string] -> [list-of letter-count]
+;letter-count-list whose letter in _letters_ start the word in _dict_
+(define (count-by-letter-help letters dict)
+  (match letters
+    ['() '()]
+    [(cons head tail)
+     (let ([count (start-with# head dict)]
+           [result (count-by-letter-help tail dict)])
+       (if (= count 0)
+           result
+           (cons (letter-count head count) result)))]))
 
+(define (count-by-letter.v2 dict)
+  (count-by-letter-help LETTERS dict))
+
+(check-expect (count-by-letter.v2 '()) '())
+(check-expect (count-by-letter.v2 (list "apple")) (list (letter-count "a" 1)))
+(check-expect (count-by-letter.v2 dict) (list (letter-count "a" 2) (letter-count "b" 1)))
+
+;[list-of letter-count] -> letter-count
+;the letter-count in _lcs_ whose count is bigger 
+(define (most-frequent-help lcs)
+  (match lcs
+    ['() '()]
+    [(cons head '()) head]
+    [(cons head tail) (match-let* ([result (most-frequent-help tail)]
+                                   [(letter-count _ head-count) head]
+                                   [(letter-count _ result-count) result])
+                        (if (>= head-count result-count) head result))]))
+
+;dictionary -> letter-count
+;letter-count most frequent letter that start-with words in _dict_
+(define (most-frequent dict)
+  (most-frequent-help (count-by-letter.v2 dict)))
+
+(check-expect (most-frequent (list "banana")) (letter-count "b" 1))
+(check-expect (most-frequent dict) (letter-count "a" 2))
 (test)
