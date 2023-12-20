70c881b46fab479f3d54936195e0860238f5e5b0
ihaaae <5929624d@gmail.com>
2023-12-20 11:30:22 +0800

    All Exercise of Project XML before Exercise 366
    I forget what I have done, so I comment some code.
    I will uncomment, test and commit them exercise by exercise.

--- XML.rkt
+++ XML.rkt
@@ -1,4 +1,5 @@
 #lang racket
+(require test-engine/racket-tests)
 ;; An Xexpr.v2 is a list:
 ;; -(cons Symbol Body)
 ;; where Body is the following
@@ -7,21 +8,21 @@
 ;; An Atrribute is a list of two items:
 ;; (cons Symbol (cons String '()))
 
-(define (temp xex)
-  (let ([optional-loa+content (rest xex)])
-    (cond
-      [(empty? optional-loa+content) '()]
-      [else (let ([loa-or-x (first optional-loa+content)])
-              (if (list-of-attributes? loa-or-x)
-                  ... (rest loa-or-x) ...
-                  ... loa-or-x ...))])))
+;; (define (temp xex)
+;;   (let ([optional-loa+content (rest xex)])
+;;     (cond
+;;       [(empty? optional-loa+content) '()]
+;;       [else (let ([loa-or-x (first optional-loa+content)])
+;;               (if (list-of-attributes? loa-or-x)
+;;                   ... (rest loa-or-x) ...
+;;                   ... loa-or-x ...))])))
 
 ;; <transition from="seen-e" to="seen-f" /> in Xexpr.v2
 (define transition (cons 'transition
                          (cons (list (cons 'from (cons "seen-e" '()))
                                      (cons 'to (cons "seen-f" '())))
                                '())))
-(define transition-two '(trainsition ((from "seen-e") (to "seen-f"))))
+(define transition-two '(transition ((from "seen-e") (to "seen-f"))))
 
 ;; <word /> in Xexpr.v2
 (define word (cons 'word '()))
@@ -33,7 +34,7 @@
                                     (list word)))))
 (define xexpr-one-two '(ul (li (word) (word)) (li (word))))
 
-(check-expect trainstion transition-two)
+(check-expect transition transition-two)
 (check-expect xexpr-one xexpr-one-two)
 
 (define xexpr-two'(server ((name "example.org"))))
@@ -45,20 +46,20 @@
                                             (cons 'player
                                                   (cons (list (cons 'name (cons "sam" '())))
                                                         '())))))
-(check-expect xexpr-three expr-three-two)
+(check-expect xexpr-three xexpr-three-two)
 ;; correspond to <carcas> <board><grass /></ board> <player name="same"></ carcas>
 (define xexpr-b '(start)) ;xexpr.v0
 ;; correspond to <start />
 
 (define a0 '((initial "X")))
- 
+
 (define e0 '(machine))
 (define e1 `(machine ,a0))
 (define e2 '(machine (action)))
 (define e3 '(machine () (action)))
 (define e4 `(machine ,a0 (action) (action)))
 
-; Xexpr.v2 -> [List-of Attribute]
+; Xexpr.v2 -> [List-of Attribute
 ; retrieves the list of attributes of xe
 (define (xexpr-attr xe)
   (local ((define optional-loa+content (rest xe)))
@@ -86,40 +87,42 @@
      (local ((define possible-attribute (first x)))
        (cons? possible-attribute))]))
 
-; Xexpr.v2 -> symbol
-(define (xexpr-name xe)
-  (first xe))
+;; ; Xexpr.v2 -> symbol
+;; (define (xexpr-name xe)
+;;   (first xe))
 
-(check-expect (xexpr-name e0) 'machine)
-(check-expect (xexpr-name e1) 'machine)
-(check-expect (xexpr-name e2) 'machine)
-(check-expect (xexpr-name e3) 'machine)
-(check-expect (xexpr-name e4) 'machine)
+;; (check-expect (xexpr-name e0) 'machine)
+;; (check-expect (xexpr-name e1) 'machine)
+;; (check-expect (xexpr-name e2) 'machine)
+;; (check-expect (xexpr-name e3) 'machine)
+;; (check-expect (xexpr-name e4) 'machine)
 
-; Xerpr.v2 -> [List-of Xexpr.v2]
-(define (xexpr-content ex)
-  (let [optional-loa+content (rest ex)]
-    (cond
-      [(empty? optional-loa+content) '()]
-      [else (let [loa-or-x (first optional-loa+content)]
-              (if (list-of-attributes? loa-or-x)
-                  (rest optional-loa+content)
-                  optional-loa+content))])))
+;; ; Xerpr.v2 -> [List-of Xexpr.v2]
+;; (define (xexpr-content ex)
+;;   (let [optional-loa+content (rest ex)]
+;;     (cond
+;;       [(empty? optional-loa+content) '()]
+;;       [else (let [loa-or-x (first optional-loa+content)]
+;;               (if (list-of-attributes? loa-or-x)
+;;                   (rest optional-loa+content)
+;;                   optional-loa+content))])))
 
-(check-expect (xexpr-content e0) '())
-(check-expect (xexpr-content e1) '())
-(check-expect (xexpr-content e2) '((action)))
-(check-expect (xexpr-content e3) '((action)))
-(check-expect (xexpr-content e4) '((action) (action)))
+;; (check-expect (xexpr-content e0) '())
+;; (check-expect (xexpr-content e1) '())
+;; (check-expect (xexpr-content e2) '((action)))
+;; (check-expect (xexpr-content e3) '((action)))
+;; (check-expect (xexpr-content e4) '((action) (action)))
 
-;[List-of Attributes] Symbol -> {string or #false}
-(define (find-attr attributes-list the-attribute)
-  (if (boolean? (assq the-attribute attributes-list))
-      #false
-      (second (assq the-attribute attributes-list))))
+;; ;[List-of Attributes] Symbol -> {string or #false}
+;; (define (find-attr attributes-list the-attribute)
+;;   (if (boolean? (assq the-attribute attributes-list))
+;;       #false
+;;       (second (assq the-attribute attributes-list))))
 
-(check-expect (find-attr (xexpr-attr e0) 'initial) #false)
-(check-expect (find-attr (xexpr-attr e1) 'initial) "X")
-(check-expect (find-attr (xexpr-attr e2) 'initial) #false)
-(check-expect (find-attr (xexpr-attr e3) 'initial) #false)
-(check-expect (find-attr (xexpr-attr e4) 'initial) "X")
+;; (check-expect (find-attr (xexpr-attr e0) 'initial) #false)
+;; (check-expect (find-attr (xexpr-attr e1) 'initial) "X")
+;; (check-expect (find-attr (xexpr-attr e2) 'initial) #false)
+;; (check-expect (find-attr (xexpr-attr e3) 'initial) #false)
+;; (check-expect (find-attr (xexpr-attr e4) 'initial) "X")
+
+(test)
