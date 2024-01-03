7c49a337ec2f74eb7e6a4e66911482045282016b
ihaaae <5929624d@gmail.com>
2024-01-03 12:16:45 +0800

    Exercise 481
    I skip Exercise 480 since revising Graphic drawing in racket is so boring.

--- Queens.rkt
+++ Queens.rkt
@@ -31,3 +31,25 @@
   (check-equal? (threatening? 2-3 6-3) #true)
   (check-equal? (threatening? 2-3 3-2) #true)
   (check-equal? (threatening? 2-3 3-4) #true))
+
+;; Exercise 481
+;; [List-of QP] -> Boolean
+;; N -> [[List-of QP] -> Boolean]
+(define (n-queens-solution? n)
+  ;; QP [List-of QP] -> Boolean
+  (define (threatening?/list qp-one qp-list)
+    (ormap (lambda (qp-two) (threatening? qp-one qp-two)) qp-list))
+  (lambda (qp-list)
+    (cond
+      [(not (= (length qp-list) n)) #false]
+      [else (not (ormap (lambda (qp-one)
+                          (threatening?/list qp-one (remove qp-one qp-list)))
+                        qp-list))])))
+
+(module+ test
+  (define 0-1 (posn 0 1))
+  (define 1-3 (posn 1 3))
+  (define 2-0 (posn 2 0))
+  ;; (define 3-2 (make-posn 3 2))
+  (check-equal? ((n-queens-solution? 4) (list 0-1 1-3 2-0 3-2)) #true))
+
