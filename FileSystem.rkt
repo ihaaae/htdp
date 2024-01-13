d0e9e559f4902c8da07883055d357d19820eac01
ihaaae <5929624d@gmail.com>
2024-01-13 15:19:38 +0800

    Exercise 331

--- FileSystem.rkt
+++ FileSystem.rkt
@@ -1,5 +1,5 @@
 #lang racket
-
+(require rackunit)
 (require scribble/srcdoc
          (for-doc racket/base scribble/manual))
 
@@ -23,3 +23,17 @@
 (define part3 "part3")
 (define Text (cons part1 (cons part2 (cons part3 '()))))
 (define TS (cons Text (cons read! (cons Libs '()))))
+
+;; Exercise 331
+;; Dir.v1 -> Number
+;; return the number of files in dir
+(define (how-many dir)
+  (cond
+    [(empty? dir) 0]
+    [(string? (first dir)) (+ 1 (how-many (rest dir)))]
+    [else (+ (how-many (first dir))
+             (how-many (rest dir)))]))
+
+(module+ test
+  (check-equal? (how-many Libs) 3)
+  (check-equal? (how-many TS) 7))
